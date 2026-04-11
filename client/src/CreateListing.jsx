f83f2d5b0d45250f3535495e3cfbd1ad0f3c2212
mujib77 <mujibkhanpathan6@gmail.com>
2026-04-11 20:58:50 +0530

    base url changes

--- client/src/CreateListing.jsx
+++ client/src/CreateListing.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 
-const BASE_URL = "https://hackmate-production.up.railway.app"
+const BASE_URL = import.meta.env.VITE_BASE_URL || 'http://localhost:5000';
 
 function CreateListing() {
     const [hackathonName, setHackathonName] = useState("");
